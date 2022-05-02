e4032a49403405fa1c5bd62d39eb633e330034c1
snl95 <snl95@txstate.edu>
2022-05-02 12:12:51 -0500

    svelte poke-dex

---
+++ src/lib/pokeCard.svelte
@@ -0,0 +1,11 @@
+<script>
+	export let pokemon;
+</script>
+
+<a
+	class="p-6 bg-gray-100 text-gray-800 text-center rounded-md shadow-sm hover:shadow-md flex flex-col items-center"
+	href={`/pokemon/${pokemon.id}`}
+>
+	<h2 class="uppercase text-2xl">{pokemon.name}</h2>
+	<img src={pokemon.image} alt={pokemon.name} class="h-40 w-40" />
+</a>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import '../app.css';
+	import Nav from '$lib/Nav.svelte';
+</script>
+
+<div class="p-8 max-w-6xl mx-auto">
+	<Nav class="flex justify-center w-full" />
+	<slot />
+</div>
